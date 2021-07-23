960b882a2e98e4bc422ae771ebddbdf337856e7e
Gilmar de Alcantara <gilmar.alcantara@hotmart.com>
2021-07-22 22:18:39 -0300

    creage get topic by id and refactoring topic service

--- src/main/kotlin/br/com/alura/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/controller/TopicController.kt
@@ -4,7 +4,9 @@ import br.com.alura.model.Course
 import br.com.alura.model.Topic
 import br.com.alura.model.User
 import br.com.alura.service.TopicService
+import org.apache.juli.logging.Log
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
@@ -18,5 +20,10 @@ class TopicController(
     @GetMapping
     fun list(): List<Topic> {
         return service.getList()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): Topic {
+        return service.getById(id)
     }
 }
--- src/main/kotlin/br/com/alura/service/TopicService.kt
+++ src/main/kotlin/br/com/alura/service/TopicService.kt
@@ -4,14 +4,17 @@ import br.com.alura.model.Course
 import br.com.alura.model.Topic
 import br.com.alura.model.User
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 
 @Service
 class TopicService {
-    fun getList(): List<Topic> {
-        val topic = Topic(
+    private val topics: List<Topic>
+
+    init {
+        val topic1 = Topic(
             id = 1,
-            title = "Kotlin Duvida 2",
-            message = "Variable in kotlin lang",
+            title = "Kotlin Duvida 1",
+            message = "Variable in kotlin lang 1",
             course = Course(
                 id = 1,
                 name = "Kotlin",
@@ -23,6 +26,44 @@ class TopicService {
                 email = "maria22@email.com"
             )
         )
-        return listOf(topic, topic, topic)
+        val topic2 = Topic(
+            id = 2,
+            title = "Kotlin Duvida 2",
+            message = "Variable in kotlin lang 2",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "programing"
+            ),
+            author = User(
+                id = 1,
+                name = "Maria 22",
+                email = "maria22@email.com"
+            )
+        )
+        val topic3 = Topic(
+            id = 3,
+            title = "Kotlin Duvida 3",
+            message = "Variable in kotlin lang 3",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "programing"
+            ),
+            author = User(
+                id = 1,
+                name = "Maria 22",
+                email = "maria22@email.com"
+            )
+        )
+        topics = listOf(topic1, topic2, topic3)
+    }
+
+    fun getList(): List<Topic> {
+        return topics
+    }
+
+    fun getById(id: Long): Topic {
+        return topics.find { it.id == id } ?: throw IllegalArgumentException("Id not found")
     }
 }
