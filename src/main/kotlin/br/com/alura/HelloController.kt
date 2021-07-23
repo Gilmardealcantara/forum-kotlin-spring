682d867294dfee3c45ea6957be03181654afe96a
Gilmar de Alcantara <gilmar.alcantara@hotmart.com>
2021-07-22 21:40:59 -0300

    init forum project

---
+++ src/main/kotlin/br/com/alura/HelloController.kt
@@ -0,0 +1,12 @@
+package br.com.alura
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+    @GetMapping
+    fun get() = "Hello Word maria 2"
+}
