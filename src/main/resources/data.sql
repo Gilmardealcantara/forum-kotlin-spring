d870e991b0850816139f9d51006a3dbe835c4826
Gilmar de Alcantara <gilmar.alcantara@hotmart.com>
2021-10-12 18:55:22 -0300

    polulate db with data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
 show tables;
-select * from course;
--- insert into course(id, name, category) values (1, 'kotlin', 'programing');
--- insert into user(id, name, email) values (1, 'Ana silva', 'ana@email.com');
+insert into course(id, name, category) values (1, 'kotlin', 'programing');
+insert into user(id, name, email) values (1, 'Ana silva', 'ana@email.com');
