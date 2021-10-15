326472fe507faa3a8eab941f95c7bd641b8129df
Gilmar de Alcantara <gilmar.alcantara@hotmart.com>
2021-10-14 22:17:21 -0300

    implements base auth with spring security

--- src/main/kotlin/br/com/alura/service/UserService.kt
+++ src/main/kotlin/br/com/alura/service/UserService.kt
@@ -2,9 +2,18 @@ package br.com.alura.service
 
 import br.com.alura.model.User
 import br.com.alura.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
+import java.lang.RuntimeException
 
 @Service
-class UserService(private val repository: UserRepository) {
+class UserService(private val repository: UserRepository)     : UserDetailsService
+{
     fun getById(id: Long): User = repository.getById(id)
+
+    override fun loadUserByUsername(email: String): UserDetails {
+       val user = repository.findByEmail(email) ?: throw RuntimeException("user not found")
+        return UserDetail(user)
+    }
 }
---
+++ src/main/resources/db/migration/V6__insert_password_user.sql
@@ -0,0 +1,1 @@
+update user set password='$2a$12$c1lcuU9ejX2rvWvCZGS25e6c0rpdxmnxqJPkZuybAWIS5GqLoBKgC' where id=1;
